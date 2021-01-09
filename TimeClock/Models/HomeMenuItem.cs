1816fc8c75a8c60224f82e6497b1d36d0ff80d59
Andrew Herd <aherd2985@gmail.com>
2021-01-09 09:10:15 -0600

    Setting things up

---
+++ TimeClock/Models/HomeMenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeClock.Models
+{
+  public enum MenuItemType
+  {
+    About,
+    TimeClock
+  }
+  public class HomeMenuItem
+  {
+    public MenuItemType Id { get; set; }
+
+    public string Title { get; set; }
+  }
+}
--- TimeClock/Models/TimeClockItem.cs
+++ TimeClock/Models/TimeClockItem.cs
@@ -1,14 +1,16 @@
 ﻿using SQLite;
+using System;
 
 namespace TimeClock
 {
-    public class TimeClockItem
-    {
-        [PrimaryKey, AutoIncrement]
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string Notes { get; set; }
-        public bool Done { get; set; }
-    }
+  public class TimeClockItem
+  {
+    [PrimaryKey, AutoIncrement]
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Notes { get; set; }
+    public bool Done { get; set; }
+    public DateTime? TimePunch { get; set; }
+    public string gpsLocation { get; set; }
+  }
 }
-
