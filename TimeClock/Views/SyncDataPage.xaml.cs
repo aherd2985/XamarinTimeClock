b158ec69655507c77f16585cf1aac597e4cfdfa3
aherd2985 <andrew.herd@okstate.edu>
2021-01-17 21:14:31 -0600

    Added loading screenshot

---
+++ TimeClock/Views/SyncDataPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TimeClock.Views
+{
+  public partial class SyncDataPage : ContentPage
+  {
+    public SyncDataPage()
+    {
+      InitializeComponent();
+    }
+  }
+}
