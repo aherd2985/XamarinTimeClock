2783859aa3dd389bde5d4d2f04a6dcdecb3aa55b
aherd2985 <andrew.herd@okstate.edu>
2021-01-17 20:56:47 -0600

    Added functionality to push button and call or email

--- TimeClock/Views/EmployeeContactPage.xaml.cs
+++ TimeClock/Views/EmployeeContactPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace TimeClock.Views
@@ -10,6 +10,48 @@ namespace TimeClock.Views
     public EmployeeContactPage()
     {
       InitializeComponent();
+    }
+
+    void PhoneDialerClicked(System.Object sender, System.EventArgs e)
+    {try { 
+      PhoneDialer.Open("15557432222");
+
+      }
+      catch (ArgumentNullException anEx)
+      {
+        // Number was null or white space
+      }
+      catch (FeatureNotSupportedException ex)
+      {
+        // Phone Dialer is not supported on this device.
+      }
+      catch (Exception ex)
+      {
+        // Other error has occurred.
+      }
+    }
+
+    void EmailerClicked(System.Object sender, System.EventArgs e)
+    {
+      List<string> recipients = new List<string>();
+      string useremail = "test@gmail.com";
+      recipients.Add(useremail);
+      try
+      {
+        var message = new EmailMessage
+        {
+          //Subject = subject,
+          //Body = body,
+          To = recipients
+          //Cc = ccRecipients,
+          //Bcc = bccRecipients
+        };
+        Email.ComposeAsync(message);
+      }
+      catch (Exception ex)
+      {
+        //Debug.WriteLine("Exception:>>" + ex);
+      }
     }
   }
 }
